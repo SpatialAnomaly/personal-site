5946603ef14cd93fc6ea927cc49a0cbee129778c
Rory Hurlburt <me@roryhurlburt.com>
2024-07-23 12:04:00 -0400

    Trying to get the airtable token to work

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -12,7 +12,7 @@ function sendForm(formData: FormData) {
   console.log(formData)
 
   const airtableUrl = 'https://api.airtable.com/v0/appyakULO8J61yyOn/tblF1CQMhtRuqCO8E'
-  const airtableApiKey = process.env.AIRTABLE_API_KEY
+  const airtableApiKey = process.env.VITE_AIRTABLE_API_KEY
 
   axios
     .post(
